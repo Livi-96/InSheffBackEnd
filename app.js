42798032555b3c67d214c6865b85b2d920b23d65
Livi-96 <Olivia.1996@outlook.com>
2024-01-25 22:44:44 +0000

    checking .env

--- app.js
+++ app.js
@@ -14,10 +14,11 @@ const PORT = process.env.PORT ?? 7001;
 const HOST = process.env.HOST ?? '0.0.0.0';
 
 app.listen(PORT, HOST, () => {
-  console.log(`Server listening on port http://localhost:${PORT}`);
+  console.log(`Server listening on port: http://localhost:${PORT}`);
 });
 
 app.get("/", getAll)
 
 
 app.post("/", addMarker)
+
